9bcac171af4a2bd194ce4db42221348553347e14
Abhiroopa Mathiyuki Manohar <60169171+Abhiroopa25@users.noreply.github.com>
2026-09-17 00:33:18 +0530

    Add files via upload

---
+++ Day1/homeAssignment1.js
@@ -0,0 +1,13 @@
+const browserVersion ="Chrome"
+
+function getBrowserVersion(){
+    
+    if(browserVersion==="Chrome")
+    {
+        //var browserVersion="edge"
+       let browserVersion ="edge"
+    }
+    console.log(browserVersion)
+
+}getBrowserVersion()
+
